f3ba7bb98d6eb08b44acda964777a455c207886f
Pim van Die <pim.vandie@iodigital.com>
2024-08-19 17:12:46 +0200

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -8,9 +8,7 @@ export default defineConfig({
   plugins: [vue()],
 
   base: '/turntable-presentation/',
-  build: {
-    outDir: 'scratch-townhall'
-  },
+
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
